ca79954c214b2eaff5699aa25826ad531d83314d
fikiUbuntu <romanrafiq456@gmail.com>
2019-08-09 16:52:13 +0200

    first commit

---
+++ src/main/java/com/tourist/index/touristindex/TouristIndexApplication.java
@@ -0,0 +1,13 @@
+package com.tourist.index.touristindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristIndexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristIndexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tourist/index/touristindex/TouristIndexApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tourist.index.touristindex;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TouristIndexApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
